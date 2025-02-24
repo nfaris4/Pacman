d71569b048a922accf8faca2c9099203b1a38e8e
nfaris4 <nfaris@inscaparrella.cat>
2025-02-24 17:15:59 +0100

    cambiar imatges resize

--- js/sketch2.js
+++ js/sketch2.js
@@ -4,7 +4,6 @@ import { Food } from "./classes/food.js";
 import { configGame } from "./constants.js";
 import { ErrorPac } from "./classes/errorPac.js";
 import { Powup } from "./classes/powup.js";
-import { IMAGE_SIZE } from "./sketch.js";
 
 let imgRock;
 let numberImagesLoaded = 0;
@@ -23,12 +22,12 @@ const arrPowerUp = [];
 
 function preload() {
   imgRock = loadImage("../media/roca.png", handleImage, handleError);
-  imgFood = loadImage("../media/all.png", handleImage, handleError);
-  imgPacman = loadImage("../media/simonLeft.png", handleImage, handleError);
-  imgPacmanRight = loadImage("../media/simonRight.png", handleImage, handleError);
-  imgPacmanUp = loadImage("../media/simoncUp.png", handleImage, handleError);
-  imgPacmanLeft = loadImage("../media/simonLeft.png", handleImage, handleError);
-  imgPacmanDown = loadImage("../media/simonDown.png", handleImage, handleError);
+  imgFood = loadImage("../media/all32.png", handleImage, handleError);
+  imgPacman = loadImage("../media/simonLeft32.png", handleImage, handleError);
+  imgPacmanRight = loadImage("../media/pacRight32.png", handleImage, handleError);
+  imgPacmanUp = loadImage("../media/simonUp32.png", handleImage, handleError);
+  imgPacmanLeft = loadImage("../media/simonLeft32.png", handleImage, handleError);
+  imgPacmanDown = loadImage("../media/simonDown32.png", handleImage, handleError);
   imgPowerUp = loadImage("../media/powerupimg.png", handleImage, handleError);
   wakaSound = loadSound("../media/audio/WakaWaka.mp3", handleErrorSound);
 }
